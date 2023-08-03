a3ce2f84be7e636c5d96b84a4448bc933a622865
Mecopi <mecopilive@gmail.com>
2023-08-03 09:47:45 +0200

    feat(string-utils): add safe slice method

--- src/string.spec.ts
+++ src/string.spec.ts
@@ -4,4 +4,8 @@ describe('String utils Tests', () => {
   it('should return 5 !', () => {
     expect(StringUtils.getFirstLastIndexOf('Hello', 'o')).toBe(4);
   });
+
+  it('should return Hello !', () => {
+    expect(StringUtils.safeSlice('Hello', 5, 3)).toBe('lo');
+  });
 });
--- src/string.ts
+++ src/string.ts
@@ -7,4 +7,18 @@ export class StringUtils {
       if (toAnalyze[i] != sequence) return i + 1;
     }
   }
+
+  public static safeSlice(
+    stringToSlice: string,
+    fromIndex: number,
+    toIndex: number,
+  ) {
+    const highter = fromIndex > toIndex ? fromIndex : toIndex;
+    const lower = highter == fromIndex ? toIndex : fromIndex;
+
+    return stringToSlice.slice(
+      lower > stringToSlice.length ? 0 : lower < 0 ? 0 : lower,
+      highter > stringToSlice.length ? 0 : highter < 0 ? 0 : highter,
+    );
+  }
 }
